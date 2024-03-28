celo-org/optimism

Add MVP EigenDA Support
Features:
* Supports writing framesets to EigenDA, and then posting frameset EigenDA "references"
  to L1 calldata.
* If the write of a frameset to EigenDA fails, then the original
  frameset is written to L1 calldata. This graceful fallback is
  supported by a new wrapper type that encapsulates the union type between
  raw frameset data and a frameset reference, and is implemented using
  protocol buffers.
* Reading is transparently handled on the derivation pipeline side.

Caveats:
* The introduction of the concept of external data availability probably
  demands a refactor and reorganization of some logic. This changeset
  does not attempt any of this, instead inserting hacky logic in sometimes
  undesirable places to achieve this the intended proof-of-concept goal.
* op-programs in now broken an needs update to support "off-chain" DA.
* op-e2e is also broken and will need fixing before end-to-end tests
  can be run successfully.

op-node/rollup/derive/calldata_source.go
@@ -2,17 +2,23 @@ package derive
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 
+	"github.com/Layr-Labs/eigenda/api/grpc/retriever"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
+	"google.golang.org/protobuf/proto"
 
+	"github.com/ethereum-optimism/optimism/op-node/da"
 	"github.com/ethereum-optimism/optimism/op-node/rollup"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/ethereum-optimism/optimism/op-service/proto/gen/op_service/v1"
 )
 
 type DataIter interface {
@@ -30,15 +36,16 @@ type DataSourceFactory struct {
 	log     log.Logger
 	cfg     *rollup.Config
 	fetcher L1TransactionFetcher
+	daCfg   *da.DAConfig
 }
 
-func NewDataSourceFactory(log log.Logger, cfg *rollup.Config, fetcher L1TransactionFetcher) *DataSourceFactory {
-	return &DataSourceFactory{log: log, cfg: cfg, fetcher: fetcher}
+func NewDataSourceFactory(log log.Logger, cfg *rollup.Config, daCfg *da.DAConfig, fetcher L1TransactionFetcher) *DataSourceFactory {
+	return &DataSourceFactory{log: log, cfg: cfg, daCfg: daCfg, fetcher: fetcher}
 }
 
 // OpenData returns a DataIter. This struct implements the `Next` function.
 func (ds *DataSourceFactory) OpenData(ctx context.Context, id eth.BlockID, batcherAddr common.Address) DataIter {
-	return NewDataSource(ctx, ds.log, ds.cfg, ds.fetcher, id, batcherAddr)
+	return NewDataSource(ctx, ds.log, ds.cfg, ds.daCfg, ds.fetcher, id, batcherAddr)
 }
 
 // DataSource is a fault tolerant approach to fetching data.
@@ -55,11 +62,12 @@ type DataSource struct {
 	log     log.Logger
 
 	batcherAddr common.Address
+	daCfg       *da.DAConfig
 }
 
 // NewDataSource creates a new calldata source. It suppresses errors in fetching the L1 block if they occur.
 // If there is an error, it will attempt to fetch the result on the next call to `Next`.
-func NewDataSource(ctx context.Context, log log.Logger, cfg *rollup.Config, fetcher L1TransactionFetcher, block eth.BlockID, batcherAddr common.Address) DataIter {
+func NewDataSource(ctx context.Context, log log.Logger, cfg *rollup.Config, daCfg *da.DAConfig, fetcher L1TransactionFetcher, block eth.BlockID, batcherAddr common.Address) DataIter {
 	_, txs, err := fetcher.InfoAndTxsByHash(ctx, block.Hash)
 	if err != nil {
 		return &DataSource{
@@ -69,11 +77,13 @@ func NewDataSource(ctx context.Context, log log.Logger, cfg *rollup.Config, fetc
 			fetcher:     fetcher,
 			log:         log,
 			batcherAddr: batcherAddr,
+			daCfg:       daCfg,
 		}
 	} else {
 		return &DataSource{
-			open: true,
-			data: DataFromEVMTransactions(cfg, batcherAddr, txs, log.New("origin", block)),
+			open:  true,
+			data:  DataFromEVMTransactions(cfg, daCfg, batcherAddr, txs, log.New("origin", block)),
+			daCfg: daCfg,
 		}
 	}
 }
@@ -85,7 +95,7 @@ func (ds *DataSource) Next(ctx context.Context) (eth.Data, error) {
 	if !ds.open {
 		if _, txs, err := ds.fetcher.InfoAndTxsByHash(ctx, ds.id.Hash); err == nil {
 			ds.open = true
-			ds.data = DataFromEVMTransactions(ds.cfg, ds.batcherAddr, txs, log.New("origin", ds.id))
+			ds.data = DataFromEVMTransactions(ds.cfg, ds.daCfg, ds.batcherAddr, txs, log.New("origin", ds.id))
 		} else if errors.Is(err, ethereum.NotFound) {
 			return nil, NewResetError(fmt.Errorf("failed to open calldata source: %w", err))
 		} else {
@@ -104,7 +114,7 @@ func (ds *DataSource) Next(ctx context.Context) (eth.Data, error) {
 // DataFromEVMTransactions filters all of the transactions and returns the calldata from transactions
 // that are sent to the batch inbox address from the batch sender address.
 // This will return an empty array if no valid transactions are found.
-func DataFromEVMTransactions(config *rollup.Config, batcherAddr common.Address, txs types.Transactions, log log.Logger) []eth.Data {
+func DataFromEVMTransactions(config *rollup.Config, daCfg *da.DAConfig, batcherAddr common.Address, txs types.Transactions, log log.Logger) []eth.Data {
 	var out []eth.Data
 	l1Signer := config.L1Signer()
 	for j, tx := range txs {
@@ -119,6 +129,54 @@ func DataFromEVMTransactions(config *rollup.Config, batcherAddr common.Address, 
 				log.Warn("tx in inbox with unauthorized submitter", "index", j, "err", err)
 				continue // not an authorized batch submitter, ignore
 			}
+
+			calldataFrame := &op_service.CalldataFrame{}
+			err = proto.Unmarshal(tx.Data(), calldataFrame)
+			if err != nil {
+				log.Warn("unable to decode calldata frame", "index", j, "err", err)
+				return nil
+			}
+
+			switch calldataFrame.Value.(type) {
+			case *op_service.CalldataFrame_FrameRef:
+				frameRef := calldataFrame.GetFrameRef()
+				if len(frameRef.QuorumIds) == 0 {
+					log.Warn("decoded frame ref contains no quorum IDs", "index", j, "err", err)
+					return nil
+				}
+
+				log.Info("requesting data from EigenDA", "quorum id", frameRef.QuorumIds[0], "confirmation block number", frameRef.ReferenceBlockNumber)
+				blobRequest := &retriever.BlobRequest{
+					BatchHeaderHash:      frameRef.BatchHeaderHash,
+					BlobIndex:            frameRef.BlobIndex,
+					ReferenceBlockNumber: frameRef.ReferenceBlockNumber,
+					QuorumId:             frameRef.QuorumIds[0],
+				}
+				blobRes, err := daCfg.Client.RetrieveBlob(context.Background(), blobRequest)
+				if err != nil {
+					retrieveReqJSON, _ := json.Marshal(struct {
+						batch_header_hash      string
+						blob_index             uint32
+						reference_block_number uint32
+						quorum_id              uint32
+					}{
+						batch_header_hash:      base64.StdEncoding.EncodeToString(frameRef.BatchHeaderHash),
+						blob_index:             frameRef.BlobIndex,
+						reference_block_number: frameRef.ReferenceBlockNumber,
+						quorum_id:              frameRef.QuorumIds[0],
+					})
+					log.Warn("could not retrieve data from EigenDA", "request", string(retrieveReqJSON), "err", err)
+					return nil
+				}
+				log.Info("Successfully retrieved data from EigenDA", "quorum id", frameRef.QuorumIds[0], "confirmation block number", frameRef.ReferenceBlockNumber)
+				data := blobRes.Data[:frameRef.BlobLength]
+				out = append(out, data)
+			case *op_service.CalldataFrame_Frame:
+				log.Info("Successfully read data from calldata (not EigenDA)")
+				frame := calldataFrame.GetFrame()
+				out = append(out, frame)
+			}
+
 			out = append(out, tx.Data())
 		}
 	}

savvar9991/optimism-dev

op-node/rollup/derive/calldata_source.go
@@ -2,16 +2,22 @@ package derive
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 
+	"github.com/Layr-Labs/eigenda/api/grpc/retriever"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
+	"google.golang.org/protobuf/proto"
 
+	"github.com/ethereum-optimism/optimism/op-node/da"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/ethereum-optimism/optimism/op-service/proto/gen/op_service/v1"
 )
 
 // CalldataSource is a fault tolerant approach to fetching data.
@@ -28,11 +34,12 @@ type CalldataSource struct {
 	log     log.Logger
 
 	batcherAddr common.Address
+	daCfg       *da.DAConfig
 }
 
 // NewCalldataSource creates a new calldata source. It suppresses errors in fetching the L1 block if they occur.
 // If there is an error, it will attempt to fetch the result on the next call to `Next`.
-func NewCalldataSource(ctx context.Context, log log.Logger, dsCfg DataSourceConfig, fetcher L1TransactionFetcher, ref eth.L1BlockRef, batcherAddr common.Address) DataIter {
+func NewCalldataSource(ctx context.Context, log log.Logger, dsCfg DataSourceConfig, daCfg *da.DAConfig, fetcher L1TransactionFetcher, ref eth.L1BlockRef, batcherAddr common.Address) DataIter {
 	_, txs, err := fetcher.InfoAndTxsByHash(ctx, ref.Hash)
 	if err != nil {
 		return &CalldataSource{
@@ -42,11 +49,14 @@ func NewCalldataSource(ctx context.Context, log log.Logger, dsCfg DataSourceConf
 			fetcher:     fetcher,
 			log:         log,
 			batcherAddr: batcherAddr,
+			daCfg:       daCfg,
 		}
-	}
-	return &CalldataSource{
-		open: true,
-		data: DataFromEVMTransactions(dsCfg, batcherAddr, txs, log.New("origin", ref)),
+	} else {
+		return &CalldataSource{
+			open:  true,
+			data:  DataFromEVMTransactions(dsCfg, daCfg, batcherAddr, txs, log.New("origin", ref)),
+			daCfg: daCfg,
+		}
 	}
 }
 
@@ -57,7 +67,7 @@ func (ds *CalldataSource) Next(ctx context.Context) (eth.Data, error) {
 	if !ds.open {
 		if _, txs, err := ds.fetcher.InfoAndTxsByHash(ctx, ds.ref.Hash); err == nil {
 			ds.open = true
-			ds.data = DataFromEVMTransactions(ds.dsCfg, ds.batcherAddr, txs, ds.log)
+			ds.data = DataFromEVMTransactions(ds.dsCfg, ds.daCfg, ds.batcherAddr, txs, ds.log)
 		} else if errors.Is(err, ethereum.NotFound) {
 			return nil, NewResetError(fmt.Errorf("failed to open calldata source: %w", err))
 		} else {
@@ -76,11 +86,56 @@ func (ds *CalldataSource) Next(ctx context.Context) (eth.Data, error) {
 // DataFromEVMTransactions filters all of the transactions and returns the calldata from transactions
 // that are sent to the batch inbox address from the batch sender address.
 // This will return an empty array if no valid transactions are found.
-func DataFromEVMTransactions(dsCfg DataSourceConfig, batcherAddr common.Address, txs types.Transactions, log log.Logger) []eth.Data {
+func DataFromEVMTransactions(dsCfg DataSourceConfig, daCfg *da.DAConfig, batcherAddr common.Address, txs types.Transactions, log log.Logger) []eth.Data {
 	out := []eth.Data{}
-	for _, tx := range txs {
+	for j, tx := range txs {
 		if isValidBatchTx(tx, dsCfg.l1Signer, dsCfg.batchInboxAddress, batcherAddr) {
-			out = append(out, tx.Data())
+			calldataFrame := &op_service.CalldataFrame{}
+			err := proto.Unmarshal(tx.Data(), calldataFrame)
+			if err != nil {
+				log.Warn("unable to decode calldata frame", "index", j, "err", err)
+				return nil
+			}
+
+			switch calldataFrame.Value.(type) {
+			case *op_service.CalldataFrame_FrameRef:
+				frameRef := calldataFrame.GetFrameRef()
+				if len(frameRef.QuorumIds) == 0 {
+					log.Warn("decoded frame ref contains no quorum IDs", "index", j, "err", err)
+					return nil
+				}
+
+				log.Info("requesting data from EigenDA", "quorum id", frameRef.QuorumIds[0], "confirmation block number", frameRef.ReferenceBlockNumber)
+				blobRequest := &retriever.BlobRequest{
+					BatchHeaderHash:      frameRef.BatchHeaderHash,
+					BlobIndex:            frameRef.BlobIndex,
+					ReferenceBlockNumber: frameRef.ReferenceBlockNumber,
+					QuorumId:             frameRef.QuorumIds[0],
+				}
+				blobRes, err := daCfg.Client.RetrieveBlob(context.Background(), blobRequest)
+				if err != nil {
+					retrieveReqJSON, _ := json.Marshal(struct {
+						batch_header_hash      string
+						blob_index             uint32
+						reference_block_number uint32
+						quorum_id              uint32
+					}{
+						batch_header_hash:      base64.StdEncoding.EncodeToString(frameRef.BatchHeaderHash),
+						blob_index:             frameRef.BlobIndex,
+						reference_block_number: frameRef.ReferenceBlockNumber,
+						quorum_id:              frameRef.QuorumIds[0],
+					})
+					log.Warn("could not retrieve data from EigenDA", "request", string(retrieveReqJSON), "err", err)
+					return nil
+				}
+				log.Info("Successfully retrieved data from EigenDA", "quorum id", frameRef.QuorumIds[0], "confirmation block number", frameRef.ReferenceBlockNumber)
+				data := blobRes.Data[:frameRef.BlobLength]
+				out = append(out, data)
+			case *op_service.CalldataFrame_Frame:
+				log.Info("Successfully read data from calldata (not EigenDA)")
+				frame := calldataFrame.GetFrame()
+				out = append(out, frame)
+			}
 		}
 	}
 	return out